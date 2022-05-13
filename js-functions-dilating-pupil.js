166e5002859ecabb2fb47e2848974c03874a4f02
irisesunlimited <71840658+irisesunlimited@users.noreply.github.com>
2022-05-13 14:12:59 -0700

    Create js-functions-dilating-pupil.js

---
+++ js-functions-dilating-pupil.js
@@ -0,0 +1,40 @@
+function Br2565(){
+document.getElementById('option1').value=5522423;
+AddToBasket();
+}
+function B5270(){
+document.getElementById('option1').value=5522424;
+AddToBasket();
+}
+function B5266(){
+document.getElementById('option1').value=5522425;
+AddToBasket();
+}
+function Br2561(){
+document.getElementById('option1').value=5522426;
+AddToBasket();
+}
+function Br2566(){
+document.getElementById('option1').value=5522427;
+AddToBasket();
+}
+function B5264(){
+document.getElementById('option1').value=5522428;
+AddToBasket();
+}
+function Br2522(){
+document.getElementById('option1').value=5522429;
+AddToBasket();
+}
+function Br2559(){
+document.getElementById('option1').value=5522430;
+AddToBasket();
+}
+function B5265(){
+document.getElementById('option1').value=5522431;
+AddToBasket();
+}
+function B5269(){
+document.getElementById('option1').value=5522432;
+AddToBasket();
+};

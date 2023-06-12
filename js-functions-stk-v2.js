7b1fcabc49e0de6769cc8cb6666b95d5e153d95c
irisesunlimited <71840658+irisesunlimited@users.noreply.github.com>
2023-06-12 11:29:39 -0700

    Create js-functions-stk-v2.js

---
+++ js-functions-stk-v2.js
@@ -0,0 +1,336 @@
+function B41593(){
+document.getElementById('option1').value="#Iris+Button+%23:B4159%0d%0a+-3;";
+AddToBasket();
+}
+function B41981(){
+document.getElementById('option1').value="#Iris+Button+%23:B4198%0d%0a+-1;";
+AddToBasket();
+}
+function B41664(){
+document.getElementById('option1').value="#Iris+Button+%23:B4166%0d%0a+-4;";
+AddToBasket();
+}
+function B4134l1(){
+document.getElementById('option1').value="#Iris+Button+%23:B4134%0d%0a+-L1;";
+AddToBasket();
+}
+function B41961(){
+document.getElementById('option1').value="#Iris+Button+%23:B4196%0d%0a+-1;";
+AddToBasket();
+}
+function B41964(){
+document.getElementById('option1').value="#Iris+Button+%23:B4196%0d%0a+-4;";
+AddToBasket();
+}
+function B41917(){
+document.getElementById('option1').value="#Iris+Button+%23:B4191%0d%0a+-7;";
+AddToBasket();
+}
+function B41914(){
+document.getElementById('option1').value="#Iris+Button+%23:B4191%0d%0a+-4;";
+AddToBasket();
+}
+function B41911(){
+document.getElementById('option1').value="#Iris+Button+%23:B4191%0d%0a+-1;";
+AddToBasket();
+}
+function B40544e(){
+document.getElementById('option1').value="#Iris+Button+%23:B4054%0d%0a+-4e;";
+AddToBasket();
+}
+function B40541e(){
+document.getElementById('option1').value="#Iris+Button+%23:B4054%0d%0a+-1e;";
+AddToBasket();
+}
+function B41931(){
+document.getElementById('option1').value="#Iris+Button+%23:B4193%0d%0a+-1;";
+AddToBasket();
+}
+function B41644(){
+document.getElementById('option1').value="#Iris+Button+%23:B4164%0d%0a+-4;";
+AddToBasket();
+}
+function B41615(){
+document.getElementById('option1').value="#Iris+Button+%23:B4161%0d%0a+-5;";
+AddToBasket();
+}
+function B40834(){
+document.getElementById('option1').value="#Iris+Button+%23:B4083%0d%0a+-4;";
+AddToBasket();
+}
+function B40831(){
+document.getElementById('option1').value="#Iris+Button+%23:B4083%0d%0a+-1;";
+AddToBasket();
+}
+function B40814(){
+document.getElementById('option1').value="#Iris+Button+%23:B4081%0d%0a+-4;";
+AddToBasket();
+}
+function B40811(){
+document.getElementById('option1').value="#Iris+Button+%23:B4081%0d%0a+-1;";
+AddToBasket();
+}
+function B42021(){
+document.getElementById('option1').value="#Iris+Button+%23:B4202%0d%0a+-1;";
+AddToBasket();
+}
+function B4202l4(){
+document.getElementById('option1').value="#Iris+Button+%23:B4202%0d%0a+-L4;";
+AddToBasket();
+}
+function B41921(){
+document.getElementById('option1').value="#Iris+Button+%23:B4192%0d%0a+-1;";
+AddToBasket();
+}
+function B40922(){
+document.getElementById('option1').value="#Iris+Button+%23:B4092%0d%0a+-2;";
+AddToBasket();
+}
+function B41166(){
+document.getElementById('option1').value="#Iris+Button+%23:B4116%0d%0a+-6;";
+AddToBasket();
+}
+function B41162(){
+document.getElementById('option1').value="#Iris+Button+%23:B4116%0d%0a+-2;";
+AddToBasket();
+}
+function B41951(){
+document.getElementById('option1').value="#Iris+Button+%23:B4195%0d%0a+-1;";
+AddToBasket();
+}
+function B41954(){
+document.getElementById('option1').value="#Iris+Button+%23:B4195%0d%0a+-4;";
+AddToBasket();
+}
+function B41957(){
+document.getElementById('option1').value="#Iris+Button+%23:B4195%0d%0a+-7;";
+AddToBasket();
+}
+function B40581(){
+document.getElementById('option1').value="#Iris+Button+%23:B4058%0d%0a+-1;";
+AddToBasket();
+}
+function B40584(){
+document.getElementById('option1').value="#Iris+Button+%23:B4058%0d%0a+-4;";
+AddToBasket();
+}
+function B40587(){
+document.getElementById('option1').value="#Iris+Button+%23:B4058%0d%0a+-7;";
+AddToBasket();
+}
+function B41891(){
+document.getElementById('option1').value="#Iris+Button+%23:B4189%0d%0a+-1;";
+AddToBasket();
+}
+function Br21363(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2136%0d%0a+-3;";
+AddToBasket();
+}
+function B40091e(){
+document.getElementById('option1').value="#Iris+Button+%23:B4009%0d%0a+-1e;";
+AddToBasket();
+}
+function B40962(){
+document.getElementById('option1').value="#Iris+Button+%23:B4096%0d%0a+-2;";
+AddToBasket();
+}
+function Br21465(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2146%0d%0a+-5;";
+AddToBasket();
+}
+function Br21403(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2140%0d%0a+--3;";
+AddToBasket();
+}
+function B41231(){
+document.getElementById('option1').value="#Iris+Button+%23:B4123%0d%0a+-1;";
+AddToBasket();
+}
+function Br20422(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2042%0d%0a+-2;";
+AddToBasket();
+}
+function B40596(){
+document.getElementById('option1').value="#Iris+Button+%23:B4059%0d%0a+-6;";
+AddToBasket();
+}
+function B40593(){
+document.getElementById('option1').value="#Iris+Button+%23:B4059%0d%0a+-3;";
+AddToBasket();
+}
+function B40467(){
+document.getElementById('option1').value="#Iris+Button+%23:B4046%0d%0a+-7;";
+AddToBasket();
+}
+function Br21613(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2161%0d%0a+-3;";
+AddToBasket();
+}
+function Br21234(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2123%0d%0a+-4;";
+AddToBasket();
+}
+function B41014(){
+document.getElementById('option1').value="#Iris+Button+%23:B4101%0d%0a+-4;";
+AddToBasket();
+}
+function Br20653(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2065%0d%0a+-3;";
+AddToBasket();
+}
+function B41497s(){
+document.getElementById('option1').value="#Iris+Button+%23:B4149%0d%0a+-7s;";
+AddToBasket();
+}
+function B41494s(){
+document.getElementById('option1').value="#Iris+Button+%23:B4149%0d%0a+-7s;";
+AddToBasket();
+}
+function B41491s(){
+document.getElementById('option1').value="#Iris+Button+%23:B4149%0d%0a+-4s;";
+AddToBasket();
+}
+function Br21802(){
+document.getElementById('option1').value="#Iris+Button+%23:B4149%0d%0a+1s;";
+AddToBasket();
+}
+function Br20171e(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2180%0d%0a+-2;";
+AddToBasket();
+}
+function Br21961(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2017%0d%0a+-1e;";
+AddToBasket();
+}
+function Br21942(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2196%0d%0a+-1;";
+AddToBasket();
+}
+function Br20344e(){
+document.getElementById('option1').value="#Iris+Button+%23:Br+2194%0d%0a+-2;";
+AddToBasket();
+}
+function Br20341e(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2034%0d%0a+-4e;";
+AddToBasket();
+}
+function Br208512(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2034%0d%0a+-1e;";
+AddToBasket();
+}
+function Br20858(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2085%0d%0a+-12;";
+AddToBasket();
+}
+function Br20855(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2085%0d%0a+-8;";
+AddToBasket();
+}
+function Br20398(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2039%0d%0a+-8;";
+AddToBasket();
+}
+function Br20395(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2039%0d%0a+-5;";
+AddToBasket();
+}
+function Br20391(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2039%0d%0a+-1;";
+AddToBasket();
+}
+function Br21864(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2186%0d%0a+-4;";
+AddToBasket();
+}
+function Br2186l1(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2186%0d%0a+-L1;";
+AddToBasket();
+}
+function Br2186l4(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2186%0d%0a+-L4;";
+AddToBasket();
+}
+function Br21477(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2147%0d%0a+-7;";
+AddToBasket();
+}
+function Br21474(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2147%0d%0a+-4;";
+AddToBasket();
+}
+function Br21471(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2147%0d%0a+-1;";
+AddToBasket();
+}
+function Br20817(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2081%0d%0a+-7;";
+AddToBasket();
+}
+function Br20813(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2081%0d%0a+-3;";
+AddToBasket();
+}
+function Br20811(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2081%0d%0a+-1;";
+AddToBasket();
+}
+function Br20794(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2079%0d%0a+-4;";
+AddToBasket();
+}
+function Br2079l1(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2079%0d%0a+-L1;";
+AddToBasket();
+}
+function Br2079l5(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2079%0d%0a+-L5;";
+AddToBasket();
+}
+function Br21887(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2188%0d%0a+-7;";
+AddToBasket();
+}
+function Br21884(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2188%0d%0a+-4;";
+AddToBasket();
+}
+function Br21881(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2188%0d%0a+-1;";
+AddToBasket();
+}
+function Br20619(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2061%0d%0a+-9;";
+AddToBasket();
+}
+function Br20615(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2061%0d%0a+-5;";
+AddToBasket();
+}
+function Br20611(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2061%0d%0a+-1;";
+AddToBasket();
+}
+function Br206010(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2060%0d%0a+-10;";
+AddToBasket();
+}
+function Br20606(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2060%0d%0a+-6;";
+AddToBasket();
+}
+function Br20602(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2060%0d%0a+-2;";
+AddToBasket();
+}
+function Br21924(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2192%0d%0a+-4;";
+AddToBasket();
+}
+function Br21921(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2192%0d%0a+-1;";
+AddToBasket();
+}
+function Br2192l4(){
+document.getElementById('option1').value="#Iris+Button+%23:Br2192%0d%0a+-L4;";
+AddToBasket();
+};
